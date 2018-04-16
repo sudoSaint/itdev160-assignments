9b87ba48a445722acb71b47fac3d17ccb72c5f37
Matthew Dal Santo <SudoSaint@gmail.com>
2018-04-16 11:57:39 -0500

    Assignment 8

--- assignment-8/js/scripts.js
+++ assignment-8/js/scripts.js
@@ -1,107 +1,81 @@
-// Global variables
-var titleTip = 'Character limit: ';
-var titleMax = 'Max limit reached!';
-var showTooltip = false;
+var data = [
+    {
+      id: 1,
+      title: 'Time',
+      body: 'Time you enjoy wasting, was not wasted.',
+      author: 'John Lennon'
+    },
+    {
+      id: 2,
+      title: 'Pain',
+      body: 'If you are pained by any external thing, it is not this thing that disturbs you, but your own judgment about it. And it is in your power to wipe out this judgment now.',
+      author: 'Marcus Aurelius'
+    },
+    {
+      id: 3,
+      title: 'Words',
+      body: 'Some people just have a way with words, and other people ... oh ... not have way.',
+      author: 'Steve Martin'
+    },
+    {
+      id: 4,
+      title: 'Peace',
+      body: 'If everyone demanded peace instead of another television set, then there\'d be peace.',
+      author: 'John Lennon'
+    },
+    {
+      id: 5,
+      title: 'Art',
+      body: 'Art is the imposing of a pattern on experience, and our aesthetic enjoyment is recognition of the pattern.',
+      author: 'Alfred North Whitehead'
+    }
+  ];
 
-// Utility functions
-function get(element) {
-  return document.getElementById(element);
-}
+  // JQuery object variables
+  var $nav = $('#nav-container');
+  var $intro = $('#intro');
+  var $posts = $('#post-container');
 
-// Event handler function
-function showPosition (event) {
-  var screenX = get('screenX'),
-      screenY = get('screenY'),
-      pageX = get('pageX'),
-      pageY = get('pageY'),
-      clientX = get('clientX'),
-      clientY = get('clientY'),
-      tooltip = get('tooltip');
+  function initPosts() {
+    for (var i = 0; i < data.length; i++) {
+      // Create elements
+      var postId = 'post-' + data[i].id,
+        $post = $('<section class="post"></section>'),
+        $title = $('<h2 class="title"></h2>'),
+        $body = $('<blockquote></blockquote>'),
+        $author = $('<span class="author"></span>'),
+        $navItem = $('<li></li>');
 
-  screenX.textContent = event.screenX;
-  screenY.textContent = event.screenY;
-  pageX.textContent = event.pageX;
-  pageY.textContent = event.pageY;
-  clientX.textContent = event.clientX;
-  clientY.textContent = event.clientY;
+        // Add post data
+        $title.text(data[i].title);
+        $body.text(data[i].body);
+        $author.text(data[i].author);
 
-  if (showTooltip) {
-    tooltip.style.left = event.clientX + 10 + 'px';
-    tooltip.style.top = event.clientY + -20 + 'px';
+        // Add nav item data
+        $navItem.attr('id', data[i].id);
+        $navItem.text(data[i].title);
+
+        // Combine post elements
+        $post.attr('id', postId);
+        $post.append($title);
+        $post.append($body);
+        $post.append($author);
+
+        // Add post and nav elements to page
+        $posts.append($post);
+        $nav.append($navItem);
+
+        // Wire up nav item click event
+        $navItem.on('click', function() {
+          var id = $(this).attr('id');
+          $posts.children().hide();
+          $posts.find('#post-' + id).fadeIn();
+        });
+
+        // Hide all posts and show the intro
+        $posts.children('.post').hide();
+        $intro.fadeIn(1000);
+    }
   }
-}
 
-function showModal (event) {
-  // Get elements
-  var modal = get('modal-dialog');
-  var backdrop = get('modal-backdrop');
-
-  // Show
-  modal.classList.add('visible');
-  backdrop.classList.add('visible');
-}
-
-function closeModal (event) {
-  var title = get('edit-title-text');
-  var text = get('edit-content-text');
-  var modal = get('modal-dialog');
-  var backdrop = get('modal-backdrop');
-
-  // Clear text
-  title.value = '';
-  text.value = '';
-
-  //Hide modal
-  modal.classList.remove('visible');
-  backdrop.classList.remove('visible');
-}
-
-function saveContent (event) {
-  var title = get('edit-title-text');
-  var text = get('edit-content-text');
-  var content = get('display-content');
-
-  // Create content elements
-  var newTitle = document.createElement('h2');
-  var newTitleText = document.createTextNode(title.value);
-  var newContent = document.createElement('p');
-  var newContentText = document.createTextNode(text.value);
-
-  // Add elements
-  newTitle.appendChild(newTitleText);
-  newContent.appendChild(newContentText);
-  content.appendChild(newTitle);
-  content.appendChild(newContent);
-
-  closeModal();
-}
-
-// Wire up event handlers
-window.addEventListener('mousemove', showPosition);
-
-window.addEventListener('load', function () {
-  // Get elements
-  var tooltipChk = get('show-tooltip');
-  var newButton = get('new-button');
-  var cancelButton = get('cancel-button');
-  var saveButton = get('save-button');
-
-  newButton.addEventListener('click', showModal);
-  cancelButton.addEventListener('click', closeModal);
-  saveButton.addEventListener('click', saveContent);
-
-  // Hide/Show the tooltip based on
-  // whether the checkbox is checked
-  tooltipChk.addEventListener('change', function() {
-    var tooltip = get('tooltip');
-
-    if (this.checked) {
-      showTooltip = true;
-      tooltip.classList.add('visible');
-    }
-    else {
-      showTooltip = false;
-      tooltip.classList.remove('visible');
-    }
-  })
-})
+  initPosts();
